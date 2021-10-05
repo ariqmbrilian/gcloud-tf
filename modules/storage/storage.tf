046861ba29a39a6135305f25d82aeb04d6e824e3
ariqmbrilian <61818849+ariqmbrilian@users.noreply.github.com>
2021-10-05 14:03:02 +0700

    Update storage.tf

--- modules/storage/storage.tf
+++ modules/storage/storage.tf
@@ -1,1 +1,6 @@
-sto
+resource "google_storage_bucket" "storage" {
+  name          = "qwiklabs-gcp-01-ffdae0e6175c"
+  location      = "US"
+  force_destroy = true
+  uniform_bucket_level_access = true
+}
