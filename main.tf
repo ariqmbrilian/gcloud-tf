ff624be6e80ffa428c8ceff14b570c004729fcd3
ariqmbrilian <61818849+ariqmbrilian@users.noreply.github.com>
2021-10-05 13:20:03 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,9 +5,16 @@ terraform {
     }
   }
 }
+
 provider "google" {
   version = "3.5.0"
   project = var.project_id
   region  = var.region
   zone    = var.zone
+}
+
+module "instances" {
+
+  source     = "./modules/instances"
+
 }
