311c3e85d212fc595dd0237ea176aafa7e1acbed
ariqmbrilian <61818849+ariqmbrilian@users.noreply.github.com>
2021-10-05 12:31:57 +0700

    Block and provider

---
+++ main.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+    }
+  }
+}
+provider "google" {
+  version = "3.5.0"
+  project = var.project_id
+  region  = var.region
+  zone    = var.zone
+}
